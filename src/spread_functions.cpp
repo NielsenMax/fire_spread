31fec38867c2a329d5e8d76706d055f3005749bd
nielsenmax <maxianielsen@gmail.com>
2025-03-26 21:24:02 -0300

    wip

--- src/spread_functions.cpp
+++ src/spread_functions.cpp
@@ -43,106 +43,84 @@ Fire simulate_fire(
     SimulationParams params, double distance, double elevation_mean, double elevation_sd,
     double upper_limit = 1.0
 ) {
+  const size_t n_row = landscape.height;
+  const size_t n_col = landscape.width;
 
-  size_t n_row = landscape.height;
-  size_t n_col = landscape.width;
+  // Pre-calculate angles for neighbor positions
+  constexpr double angles[8] = { M_PI * 3 / 4, M_PI, M_PI * 5 / 4, M_PI / 2, M_PI * 3 / 2,
+                                 M_PI / 4,     0,    M_PI * 7 / 4 };
 
-  std::vector<std::pair<size_t, size_t>> burned_ids;
+  // Pre-calculate neighbor position offsets
+  constexpr int moves[8][2] = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 },
+                                { 0, 1 },   { 1, -1 }, { 1, 0 },  { 1, 1 } };
 
+  // Initialize burned cells tracking
+  std::vector<std::pair<size_t, size_t>> burned_ids(ignition_cells);
+  std::vector<size_t> burned_ids_steps = { ignition_cells.size() };
+
+  // Initialize burned cells matrix
+  Matrix<bool> burned_bin(n_col, n_row);
+  for (const auto& cell : ignition_cells) {
+    burned_bin[{ cell.first, cell.second }] = true;
+  }
+
+  // Initialize random number generator for better performance
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<double> dist(0.0, 1.0);
+
+  // Fire spread simulation
   size_t start = 0;
   size_t end = ignition_cells.size();
 
-  for (size_t i = 0; i < end; i++) {
-    burned_ids.push_back(ignition_cells[i]);
-  }
-
-  std::vector<size_t> burned_ids_steps;
-  burned_ids_steps.push_back(end);
-
-  size_t burning_size = end + 1;
-
-  Matrix<bool> burned_bin = Matrix<bool>(n_col, n_row);
-
-  for (size_t i = 0; i < end; i++) {
-    size_t cell_0 = ignition_cells[i].first;
-    size_t cell_1 = ignition_cells[i].second;
-    burned_bin[{ cell_0, cell_1 }] = 1;
-  }
-
-  while (burning_size > 0) {
+  while (end > start) { // Only continue if there are new burning cells
     size_t end_forward = end;
 
-    // Loop over burning cells in the cycle
-
-    // b is going to keep the position in burned_ids that have to be evaluated
-    // in this burn cycle
+    // Process all cells that started burning in the previous iteration
     for (size_t b = start; b < end; b++) {
-      size_t burning_cell_0 = burned_ids[b].first;
-      size_t burning_cell_1 = burned_ids[b].second;
-
+      const size_t burning_cell_0 = burned_ids[b].first;
+      const size_t burning_cell_1 = burned_ids[b].second;
       const Cell& burning_cell = landscape[{ burning_cell_0, burning_cell_1 }];
 
-      constexpr int moves[8][2] = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 },
-                                    { 0, 1 },   { 1, -1 }, { 1, 0 },  { 1, 1 } };
-
-      int neighbors_coords[2][8];
-
-      for (size_t i = 0; i < 8; i++) {
-        neighbors_coords[0][i] = int(burning_cell_0) + moves[i][0];
-        neighbors_coords[1][i] = int(burning_cell_1) + moves[i][1];
-      }
-      // Note that in the case 0 - 1 we will have size_t_MAX
-
-      // Loop over neighbors_coords of the focal burning cell
-
+      // Check all 8 neighboring cells
       for (size_t n = 0; n < 8; n++) {
+        const int neighbour_cell_0 = int(burning_cell_0) + moves[n][0];
+        const int neighbour_cell_1 = int(burning_cell_1) + moves[n][1];
 
-        int neighbour_cell_0 = neighbors_coords[0][n];
-        int neighbour_cell_1 = neighbors_coords[1][n];
-
-        // Is the cell in range?
-        bool out_of_range = 0 > neighbour_cell_0 || neighbour_cell_0 >= int(n_col) ||
-                            0 > neighbour_cell_1 || neighbour_cell_1 >= int(n_row);
-
-        if (out_of_range)
+        // Skip if out of bounds
+        if (neighbour_cell_0 < 0 || neighbour_cell_0 >= int(n_col) || neighbour_cell_1 < 0 ||
+            neighbour_cell_1 >= int(n_row)) {
           continue;
+        }
+
+        // Skip if already burned or not burnable
+        if (burned_bin[{ neighbour_cell_0, neighbour_cell_1 }]) {
+          continue;
+        }
 
         const Cell& neighbour_cell = landscape[{ neighbour_cell_0, neighbour_cell_1 }];
-
-        // Is the cell burnable?
-        bool burnable_cell =
-            !burned_bin[{ neighbour_cell_0, neighbour_cell_1 }] && neighbour_cell.burnable;
-
-        if (!burnable_cell)
+        if (!neighbour_cell.burnable) {
           continue;
+        }
 
-        constexpr double angles[8] = { M_PI * 3 / 4, M_PI, M_PI * 5 / 4, M_PI / 2, M_PI * 3 / 2,
-                                       M_PI / 4,     0,    M_PI * 7 / 4 };
-
-        // simulate fire
-        double prob = spread_probability(
+        // Calculate spread probability
+        const double prob = spread_probability(
             burning_cell, neighbour_cell, params, angles[n], distance, elevation_mean,
             elevation_sd, upper_limit
         );
 
-        // Burn with probability prob (Bernoulli)
-        bool burn = (double)rand() / (double)RAND_MAX < prob;
-
-        if (burn == 0)
-          continue;
-
-        // If burned, store id of recently burned cell and set 1 in burned_bin
-        end_forward += 1;
-        burned_ids.push_back({ neighbour_cell_0, neighbour_cell_1 });
-        burned_bin[{ neighbour_cell_0, neighbour_cell_1 }] = true;
+        // Determine if fire spreads to this cell
+        if (dist(gen) < prob) {
+          burned_ids.emplace_back(neighbour_cell_0, neighbour_cell_1);
+          burned_bin[{ neighbour_cell_0, neighbour_cell_1 }] = true;
+          end_forward++;
+        }
       }
     }
 
-    // update start and end
+    // Update indices for next iteration
     start = end;
     end = end_forward;
-    burning_size = end - start;
-
     burned_ids_steps.push_back(end);
   }
 
